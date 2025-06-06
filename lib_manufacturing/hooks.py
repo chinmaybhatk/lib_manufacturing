b669ef9252b4b4e4fd8efb28468c3a76877adecd
chinmaybhatk <chinmaybhatk@gmail.com>
2025-06-06 12:40:07 +0530

    Add hooks.py for Frappe app configuration

---
+++ lib_manufacturing/hooks.py
@@ -0,0 +1,8 @@
+from . import __version__ as app_version
+
+app_name = "lib_manufacturing"
+app_title = "LIB Manufacturing"
+app_publisher = "Chinmay Bhat"
+app_description = "Lithium-Ion Battery Manufacturing Management"
+app_email = "chinmay@example.com"
+app_license = "MIT"
